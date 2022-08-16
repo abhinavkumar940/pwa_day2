08859f1b6c7f042fe054d16ceba6d55ce7a1a02c
Abhinav Kumar <abhinavkumar940@gmail.com>
2022-08-16 16:13:04 +0530

    push notification example

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-console.log("Trying to register our service worker");
+const publicVapidKey = 'BOhEa3poaObVoG837QxrCWo7sKUL4L7pfI6ypySv5ZEzKAFV3xLOFSChLfL9BbNnCn6Ltws4xZ1OBSOBLhUpsEY';
 
 if (navigator.serviceWorker) {
     console.log("Service worker is supported");
     window.addEventListener("load", function () {
         console.log("window is loaded");
-        navigator.serviceWorker.register("/sw.js").then(register => {
+        navigator.serviceWorker.register("/sw_swr.js").then(register => {
             console.log("Service worker registered successfuly: ", register);
         })
+    });
+
+    navigator.serviceWorker.addEventListener('message', (event) => {
+        if (event.data.group === "network") {
+            if (event.data.status === 0) {
+                alert("network is offline");
+            }
+
+        }
+    });
+
+    navigator.serviceWorker.ready.then(async (registration) => {
+        const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: (publicVapidKey),
+        });
+
+        console.log({ subscription })
     })
 } else {
     console.log("Service worker is not supported");
 }
+
--- sw_swr.js
+++ sw_swr.js
@@ -4,6 +4,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", function (event) {
+    console.log("[Service worker] Install");
     event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log("Cache opened!");
@@ -12,10 +13,16 @@ self.addEventListener("install", function (event) {
     )
 });
 
+self.addEventListener("activate", function (event) {
+    console.log("[Service worker] Activate");
+})
 
 
 self.addEventListener("fetch", async (event) => {
+
     event.respondWith((async () => {
+
+        const cachedResponse = await caches.match(event.request);
         try {
             const serverResponse = await fetch(event.request);
             if (serverResponse) {
@@ -26,10 +33,17 @@ self.addEventListener("fetch", async (event) => {
                 return serverResponse;
             }
         } catch (e) {
-            const cachedResponse = await caches.match(event.request);
-            return cachedResponse;
+            const client = await clients.get(event.clientId);
+            if (client) {
+                client.postMessage({
+                    group: "network",
+                    status: 0
+                })
+            }
+
         }
 
+        return cachedResponse;
 
     })())
 
@@ -41,6 +55,11 @@ self.addEventListener("fetch", async (event) => {
 });
 
 
-self.addEventListener("message", function (message) {
-    console.log(message);
-})
+self.addEventListener('push', (e) => {
+    const data = e.data;
+    console.log(data)
+    self.registration.showNotification("data", {
+        body: "hello",
+
+    });
+});
